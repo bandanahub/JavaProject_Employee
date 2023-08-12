9855ca449abef604dff2bcc5445c52169a1725f3
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-08-12 12:13:52 -0400

    Update Employee.java
    I entered the attributes, made the attribute private according to the question and used getter and setter to access the private fields. Similarly, I have used an integer data type for the date for the more straightforward age calculation. I have also imported the calendar to calculate the age with reference to the current date.

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,35 @@
+import java.util.Calendar;
 public class Employee {
+    private String name;
+    private int birthyear;
+    private int age;
+    private double income;
+    private double occ_rate;
+
+   public Employee(String name, int birthyear , double income , String vehicle){
+       this.name = name ;
+       this.birthyear = birthyear;
+       this.age = age;
+       this.income = income;
+       this.occ_rate =occ_rate;
+   }
+    public String getName() {
+        return name;
+    }
+    public double getIncome(){
+       return income;
+    }
+    public double getOcc_rate(){
+       return occ_rate;
+    }
+    public int getBirthYear() {
+        return birthyear;
+    }
+
+    public int getAge() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        return currentYear - birthyear;
+    }
+    
+
 }
